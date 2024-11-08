fa5deb83143a038a3daa9e06247b672846db384b
Ubuntu <ubuntu@ip-172-31-37-182.ec2.internal>
2024-11-08 14:03:05 +0000

    Updated DDPM training files and configurations

--- hw5_student_starter_code/pipelines/ddpm.py
+++ hw5_student_starter_code/pipelines/ddpm.py
@@ -1,11 +1,9 @@
-
 from typing import List, Optional, Tuple, Union
 from PIL import Image
 from tqdm import tqdm
 import torch 
 import torch.nn as nn
 from utils import randn_tensor
-
 
 
 class DDPMPipeline:
@@ -19,8 +17,7 @@ class DDPMPipeline:
             self.vae = vae
             
         # NOTE: this is for CFG
-        if class_embedder is not None:
-            self.class_embedder = class_embedder
+        self.class_embedder = class_embedder if class_embedder is not None else None
 
     def numpy_to_pil(self, images):
         """
@@ -61,7 +58,7 @@ class DDPMPipeline:
         classes: Optional[Union[int, List[int]]] = None,
         guidance_scale : Optional[float] = None,
         generator: Optional[Union[torch.Generator, List[torch.Generator]]] = None,
-        device = None,
+        device=None,
     ):
         image_shape = (batch_size, self.unet.input_ch, self.unet.input_size, self.unet.input_size)
         if device is None:
@@ -69,7 +66,7 @@ class DDPMPipeline:
         
         # NOTE: this is for CFG
         if classes is not None or guidance_scale is not None:
-            assert hasattr(self, "class_embedder"), "class_embedder is not defined"
+            assert self.class_embedder is not None, "class_embedder is not defined"
         
         if classes is not None:
             # convert classes to tensor
@@ -77,63 +74,51 @@ class DDPMPipeline:
                 classes = [classes] * batch_size
             elif isinstance(classes, list):
                 assert len(classes) == batch_size, "Length of classes must be equal to batch_size"
-                classes = torch.tensor(classes, device=device)
+            classes = torch.tensor(classes, device=device)
             
-            # TODO: get uncond classes
-            uncond_classes = None 
-            # TODO: get class embeddings from classes
-            class_embeds = None 
-            # TODO: get uncon class embeddings
-            uncond_embeds = None 
+            # Get unconditional and conditional class embeddings
+            uncond_classes = torch.zeros_like(classes)  # Zero as a placeholder for "unconditional" class
+            class_embeds = self.class_embedder(classes)
+            uncond_embeds = self.class_embedder(uncond_classes)
         
-        # TODO: starts with random noise
-        image = None # randn_tensor(image_shape, generator=generator, device=device)
+        # Start with random noise
+        image = torch.randn(image_shape, generator=generator, device=device)
 
-        # TODO: set step values using set_timesteps of scheduler
-        self.scheduler = None
+        # Set step values using scheduler's set_timesteps function
+        self.scheduler.set_timesteps(num_inference_steps)
         
-        # TODO: inverse diffusion process with for loop
+        # Inverse diffusion process
         for t in self.progress_bar(self.scheduler.timesteps):
-            
-            # NOTE: this is for CFG
-            if guidance_scale is not None or guidance_scale != 1.0:
-                # TODO: implement cfg
-                model_input = None 
-                c = None 
+            # Classifier-Free Guidance (CFG) setup
+            if guidance_scale is not None and guidance_scale != 1.0:
+                # Duplicate model inputs for conditional/unconditional guidance
+                model_input = torch.cat([image, image], dim=0)
+                c = torch.cat([uncond_embeds, class_embeds], dim=0)
             else:
-                model_input = None 
-                # NOTE: leave c as None if you are not using CFG
-                c = None
+                model_input = image
+                c = None  # Leave as None if CFG is not used
             
-            # TODO: 1. predict noise model_output
-            model_output = None
+            # 1. Predict noise model output
+            model_output = self.unet(model_input, t, class_emb=c)
             
-            if guidance_scale is not None or guidance_scale != 1.0:
-                # TODO: implement cfg
+            if guidance_scale is not None and guidance_scale != 1.0:
+                # Separate the outputs for CFG
                 uncond_model_output, cond_model_output = model_output.chunk(2)
-                model_output = None
+                model_output = uncond_model_output + guidance_scale * (cond_model_output - uncond_model_output)
             
-            # TODO: 2. compute previous image: x_t -> x_t-1 using scheduler
-            image = None 
-            
+            # 2. Compute the previous image in the reverse diffusion process
+            image = self.scheduler.step(model_output, t, image)["prev_sample"]
         
-        # NOTE: this is for latent DDPM
-        # TODO: use VQVAE to get final image
+        # For latent DDPM with VAE
         if self.vae is not None:
-            # NOTE: remember to rescale your images
-            image = None 
-            # TODO: clamp your images values
-            image = None 
+            image = self.vae.decode(image / 0.18215)  # Scale as needed
+            image = image.clamp(-1, 1)  # Clamp to the valid range
         
-        # TODO: return final image, re-scale to [0, 1]
-        image = None 
+        # Rescale to [0, 1] for final output
+        image = (image + 1) / 2
         
-        # convert to PIL images
+        # Convert to PIL images
         image = image.cpu().permute(0, 2, 3, 1).numpy()
         image = self.numpy_to_pil(image)
         
         return image
-        
-
-
-
